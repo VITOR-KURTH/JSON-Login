00dc0066f4de1406d4bd4cdcfd8304f6d23ddb19
Vitor Kurth <125030121+VKURHT@users.noreply.github.com>
2023-09-27 12:41:51 -0300

    Update script_home.js

--- script_home.js
+++ script_home.js
@@ -1,5 +1,5 @@
 let usuario =  JSON.parse(localStorage.getItem("logado"));
 document.getElementById("titulo").innerHTML = "Bem vindo, "+usuario.login
 function voltar(){
-    window.location.href = "../index.html"
+    window.location.href = "index.html"
 }
