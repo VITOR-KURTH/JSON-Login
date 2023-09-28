6b082ae71f8a181d4d667b1d555959c9fe1920c8
Vitor Kurth <125030121+VKURHT@users.noreply.github.com>
2023-09-28 10:01:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,25 @@ function login() {
     let login = campoLogin.value;
     let senha = campoSenha.value;
     let mensagem = "Usuário ou senha incorreta!";
-    let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados")); if (bancoDeDados == null) {
+    let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+
+    if (bancoDeDados == null) {
         mensagem = "Nenhum usuário cadastrado até o momento";
     } else {
         for (let usuario of bancoDeDados) {
             if (usuario.login == login && usuario.senha == senha) {
                 mensagem = "Parabéns, você logou!";
                 localStorage.setItem("logado", JSON.stringify(usuario));
-                window.location.href = "./home.html";
+                window.location.href = "home.html"
                 break;
             }
         }
 
     }
-    alert(mensagem);
+    alert(mensagem)
 
 }
+
 function cadastra() {
     if (campoNovaSenha.value == campoRepSenha.value) {
         const usuario = {
@@ -34,16 +37,18 @@ function cadastra() {
         if (bancoDeDados == null) {
             bancoDeDados = [];
         }
-        if(verificaSeExiste(campoNovoLogin.value, bancoDeDados)) {
-            alert("Esse usuário ja está cadastrado")
+        if(verificaSeExiste(campoNovoLogin.value, bancoDeDados)){
+            alert("Esse login já está cadastrado.")
         }
+        else{
         bancoDeDados.push(usuario);
         localStorage.setItem("bancoDeDados", JSON.stringify(bancoDeDados));
-
+        alert("Usuário cadastrado com sucesso!")
+        }
     } else {
         alert("As senhas são diferentes!");
     }
-    alert("Usuário cadastrado com sucesso!");
+    
 }
 
 function verificaSeExiste(login, banco){
@@ -54,3 +59,13 @@ function verificaSeExiste(login, banco){
     }
     return false;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const elemento = document.querySelector(".login");
+    elemento.classList.add("mostrar-login");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const elemento = document.querySelector(".register");
+    elemento.classList.add("mostrar-register");
+});
